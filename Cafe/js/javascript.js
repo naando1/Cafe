63a778eff9a3b8091cdc5691b2c268d820d142a2
Antonius <122329850+naando1@users.noreply.github.com>
2024-04-01 11:20:32 +0700

    Add files via upload

---
+++ Cafe/js/javascript.js
@@ -0,0 +1,15 @@
+//Toggle diaktivin dulu
+const navbarNav = document.querySelector(".navbar-nav");
+
+//ketika diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik di sembarang langsung hilang
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
